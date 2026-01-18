66581cf3d192c06bdc4f9b9433bf7a0fd618cfbc
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2026-01-18 23:11:27 +0100

    Add ItemCreated event publishing

--- model/src/main/java/template/model/Item.java
+++ model/src/main/java/template/model/Item.java
@@ -1,5 +1,12 @@
 package template.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Item {
+
+    private String id;
 
 }
---
+++ producer/src/main/java/template/producer/StartupRunner.java
@@ -0,0 +1,25 @@
+package template.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import template.model.Item;
+
+@Slf4j
+@Component
+@Profile("!test")
+@RequiredArgsConstructor
+public class StartupRunner implements CommandLineRunner {
+
+    private final ItemEventPublisher itemEventPublisher;
+
+    @Override
+    public void run(String... args) {
+        var item = Item.builder().id("1").build();
+        itemEventPublisher.publishItemCreated(item);
+        log.info("ItemCreatedEvent published on startup");
+    }
+
+}
